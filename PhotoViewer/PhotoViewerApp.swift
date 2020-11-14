ae9778e2a545496f63e53490dd7243a1ac4219d5
leihao <leihao.sg@gmail.com>
2020-11-14 16:47:57 +0800

    Initial Commit

---
+++ PhotoViewer/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  PhotoViewer
+//
+//  Created by Lei on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
